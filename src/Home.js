2cdd865d16cfd7dfcc6575f1f259a624ffad7bfb
Abdifatah Muse Abdi <35341044+AbdifatahMuse@users.noreply.github.com>
2020-10-18 19:36:10 +0300

    adding navbar

---
+++ src/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home_container">
+                <img 
+                    className="home__image"
+                    src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/gateway/placement/launch/1917/EVREF_OCT20_GWBleedingHero_FT_XSite_1500X600_PV_en-GB._CB419087828_.jpg" 
+                    alt="" />
+
+                    <div className="home_row">
+                        {/* products */}
+                    </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
