b5c9a5201126a483b902ef2264f39a3612a78a98
Abdifatah Muse Abdi <35341044+AbdifatahMuse@users.noreply.github.com>
2020-10-20 20:23:57 +0300

    adding Router Dom

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import './App.css';
-import Headers from './header';
-import Home from './Home'
-
+import React from "react";
+import "./App.css";
+import Header from "./header";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     // BEM
-    <div className="App">
-      {/* header */}
-      <Headers />
-      {/* Home */}
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>I am Checkout, Smash the like button</h1>
+          </Route>
+
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
