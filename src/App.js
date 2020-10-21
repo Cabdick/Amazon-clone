27802c8a6813d8d55f8ded04447d60749fcc288f
Abdifatah Muse Abdi <35341044+AbdifatahMuse@users.noreply.github.com>
2020-10-21 20:08:16 +0300

    Add currency format and subtotal and update home

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from "react";
 import "./App.css";
-import Header from "./header";
+import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
 
 function App() {
   return (
     // BEM
     <Router>
       <div className="App">
+        <Header />
+
         <Switch>
           <Route path="/checkout">
-            <Header />
-            <h1>I am Checkout, Smash the like button</h1>
+            <Checkout />
           </Route>
 
           <Route path="/">
-            <Header />
             <Home />
           </Route>
         </Switch>
