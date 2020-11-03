75ea552e7de2cecc26a2ac546b3ff013a41c85c8
Abdifatah Muse Abdi <35341044+AbdifatahMuse@users.noreply.github.com>
2020-11-03 19:50:17 +0300

    All Functions Completed

--- src/Payment.js
+++ src/Payment.js
@@ -7,6 +7,7 @@ import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import CurrencyFormat from "react-currency-format";
 import { getBasketTotal } from "./reducer";
 import axios from "./axios";
+import { db } from "./firebase";
 
 function Payment() {
   const history = useHistory();
@@ -38,6 +39,7 @@ function Payment() {
   }, [basket]);
 
   console.log("THE SECRET IS >>> ", clientSecret);
+  console.log("THE User IS >>> ", user);
 
   const handleSubmit = async (event) => {
     // do all the fancy stripe stuff
@@ -53,6 +55,16 @@ function Payment() {
       })
       .then(({ paymentIntent }) => {
         // paymentIntent = payment confirmation
+
+        db.collection("users")
+          .doc(user?.uid)
+          .collection("orders")
+          .doc(paymentIntent.id)
+          .set({
+            basket: basket,
+            amount: paymentIntent.amount,
+            created: paymentIntent.created,
+          });
 
         setSucceeded(true);
         setError(null);
